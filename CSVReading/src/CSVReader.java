135c638a408b2208fea7a144ab27f6ac7aeaed23
LuisHBeck <beck.henrique.luis@gmail.com>
2023-05-20 14:34:20 -0300

    modifying paths

--- CSVReading/src/CSVReader.java
+++ CSVReading/src/CSVReader.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class CSVReader {
     public static void main(String[] args) {
-        String path = "C:/Users/47238341840/Desktop/e-comerce/archives/Products.csv";
+        String pathSenai = "C:/Users/47238341840/Desktop/e-comerce/archives/Products.csv";
+        // String pathHome = "C:/Users/Luis Beck/Desktop/e-comerce/archives/Products.csv";
         List<Products> productsList = new ArrayList<>();
 
-        try (BufferedReader bReader = new BufferedReader(new FileReader(path))){
+        try (BufferedReader bReader = new BufferedReader(new FileReader(pathSenai))){
             String line = bReader.readLine();
             line = bReader.readLine();
             while (line != null){
